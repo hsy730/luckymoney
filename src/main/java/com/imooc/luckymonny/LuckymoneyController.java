7f5a277896bffef398bb8b567a01157eaaeec22c
“huangsiyuan” <siyuanh640@gmail.com>
2019-12-28 10:23:45 +0800

    学习完毕，主要是初步学习四种rest请求方式，以及通过JPA库操作对数据库

--- src/main/java/com/imooc/luckymonny/LuckymoneyController.java
+++ src/main/java/com/imooc/luckymonny/LuckymoneyController.java
@@ -1,17 +1,68 @@
 package com.imooc.luckymonny;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LuckymoneyController {
     @Autowired
     private LuckymoneyRepository repository;
+
+    /**
+     * 获取红包列表
+     * @return
+     */
     @GetMapping("/luckymoneys")
     public List<Luckymoney> list() {
         return repository.findAll();
+    }
+
+    /**
+     * 创建红包，即发红包
+     */
+    @PostMapping("/luckymoneys")
+    public Luckymoney create(@RequestParam("producer") String producer,
+                             @RequestParam("money") BigDecimal money) {
+        Luckymoney luckymoney = new Luckymoney();
+        luckymoney.setProducer(producer);
+        luckymoney.setMoney(money);
+        return repository.save(luckymoney);
+    }
+
+
+    /**
+     * 通过id查询红包
+     */
+    @GetMapping("/luckymoneys/{id}")
+    public Luckymoney findById(@PathVariable("id") Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    /**
+     * 更新红包(领红包)
+     */
+    @PutMapping("/luckymoneys/{id}")
+    public Luckymoney update(@PathVariable("id") Integer id, @RequestParam("consumer") String consumer) {
+        Optional<Luckymoney> optional= repository.findById(id);
+        if (optional.isPresent()) {
+            Luckymoney luckymoney = optional.get();
+            luckymoney.setConsumer(consumer);
+            return repository.save(luckymoney);
+        }
+        return null;
+    }
+
+    /**
+     * 使用该注解让方法也具有事务的特性
+     * 但是注意数据库首先要支持事务，才能实现该功能
+     */
+    @Transactional
+    public void doSomething() {
+        // 多次操作mysql
     }
 }
